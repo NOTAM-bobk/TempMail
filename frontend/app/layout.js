8bebf4ac60e1638dab99e0f8dfc8cb7b83866ab8
NOTAM <sawyerbobk563@gmail.com>
2026-05-16 16:04:07 -0500

    Update layout.js

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,1 +1,14 @@
+import './globals.css'
 
+export const metadata = {
+  title: 'Free Temp Mail',
+  description: 'A disposable email service',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
